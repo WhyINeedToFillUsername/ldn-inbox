66a4c6dd478c2416ffbf87476ac13316ee314245
WhyINeedToFillUsername <t.karola@gmail.com>
2018-09-09 13:05:51 +0200

    changed template engine from pug to handlebars

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Inbox' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Inbox',
+        messages: ["Cras justo odio", "Dapibus ac facilisis in", "Porta ac consectetur ac", "Vestibulum at eros"]
+    });
 });
 
 module.exports = router;
