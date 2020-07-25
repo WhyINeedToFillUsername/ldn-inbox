4797db4ad73abb0043b27b29652e700d09c7ebf1
WhyINeedToFillUsername <t.karola@gmail.com>
2020-07-25 10:35:33 +0200

    target - implemented all options

--- target/routes/index.js
+++ target/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const INBOX_URL = 'http://localhost:5001/API/notifications/';
 const LINK_VALUE = '<' + INBOX_URL + '>; rel="http://www.w3.org/ns/ldp#inbox"';
 
-/* TODO https://www.w3.org/TR/ldn/#discovery:
+/* Discovery options (from https://www.w3.org/TR/ldn/#discovery):
 
  1) LINK
   a) head > Link: <http://example.org/inbox/>; rel="http://www.w3.org/ns/ldp#inbox"
@@ -18,15 +18,52 @@ const LINK_VALUE = '<' + INBOX_URL + '>; rel="http://www.w3.org/ns/ldp#inbox"';
   e) text/turtle with <http://www.w3.org/ns/ldp#inbox> relation
 */
 
-/* GET home page. */
+// 1) LINK
+//  a) head > Link: <http://example.org/inbox/>; rel="http://www.w3.org/ns/ldp#inbox"
+router.head('/', function (req, res, next) {
+    res.set('Link', LINK_VALUE);
+    res.status(200).end();
+});
+
+//  b) get  > Link: <http://example.org/inbox/>; rel="http://www.w3.org/ns/ldp#inbox"
 router.get('/', function (req, res, next) {
     res.set('Link', LINK_VALUE);
     res.render('index', {title: 'Inbox discovery demo'});
 });
 
-router.head('/', function (req, res, next) {
-    res.set('Link', LINK_VALUE);
-    res.status(200).end();
+
+// 2) RDF
+router.get('/content', function (req, res, next) {
+    // switch response based on Accept header and set the response content type accordingly
+    res.format({
+        // a) JSON with relation of type http://www.w3.org/ns/ldp#inbox
+        'application/ld+json': function () {
+            res.send(
+                {
+                    "@context": "http://www.w3.org/ns/ldp",
+                    "@id": "https://tonda.solid.community/",
+                    "inbox": "https://tonda.solid.community/inbox/"
+                }
+            )
+        },
+
+        // b) HTML <a> with rel="http://www.w3.org/ns/ldp#inbox"
+        // c) HTML <link> with rel="http://www.w3.org/ns/ldp#inbox"
+        // d) HTML <section> with property="http://www.w3.org/ns/ldp#inbox"
+        'text/html': function () {
+            res.render('contentWithRdf');
+        },
+
+        // e) text/turtle with <http://www.w3.org/ns/ldp#inbox> relation
+        'text/turtle': function () {
+            res.send("<https://tonda.solid.community/> <http://www.w3.org/ns/ldp#inbox> <https://tonda.solid.community/inbox/> .");
+        },
+
+        default: function () {
+            // log the request and respond with 406
+            res.status(406).send('Not Acceptable')
+        }
+    })
 });
 
 module.exports = router;
